ac9fcb0ec7284d50544ee69e821d9c4bccb25e40
赵亮 <ivybloosom@163.com>
2021-10-21 11:21:05 +0800

    add the very first hello world code

---
+++ 01_helloworld/main.go
@@ -0,0 +1,15 @@
+// *** hello world of go ***//
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hello world")
+}
+
+// *** End of hello world ***//
+
+// compile and create a binary / executable file :
+// (bash) go install
